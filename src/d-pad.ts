883b53dc90a2273c3cf07d476964f0211a4ed082
Jono Hill <jono@hillnz.com>
2022-10-02 11:35:00 +1300

    insta death button

--- src/d-pad.ts
+++ src/d-pad.ts
@@ -5,17 +5,17 @@ import { KEY_STATES } from './utils/keyevents';
 
 
 function wire(el: HTMLElement, keyCode: Key) {
-    el.onmousedown = function () {
+    el.onpointerdown = function () {
         KEY_STATES[keyCode] = true;
     }
-    el.onmouseup = function () {
+    el.onpointerup = function () {
         KEY_STATES[keyCode] = false;
     }
 }
 
 export function init() {
 
-    const [up, down, left, right] = [1, 2, 3, 4].map(n => {
+    const [up, down, left, right, flame] = [1, 2, 3, 4, 5].map(n => {
         const el = document.querySelector(`#touch_controls > div:nth-child(${n})`);
         assert(el, 'd-pad button element not found!');
         assert(el instanceof HTMLElement, 'expected d-pad element to be a div');
@@ -26,4 +26,5 @@ export function init() {
     wire(down, Key.DownArrow);
     wire(left, Key.LeftArrow);
     wire(right, Key.RightArrow);
+    wire(flame, Key.Space);
 }
