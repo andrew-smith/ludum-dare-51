c1a75d6cc13f7176eebb0344e2cd4d4bb69f87ab
Jono Hill <jono@hillnz.com>
2022-10-01 18:26:08 +1300

    audio

---
+++ src/audio/index.ts
@@ -0,0 +1,40 @@
+
+const AUDIO = {
+    miniExplosion: new URL('mini-explosion.mp3', import.meta.url),
+} as const;
+type Audio = { [name in keyof typeof AUDIO]: ArrayBuffer };
+
+export let GameAudio: Audio | null = null;
+
+export async function loadAudio() {
+
+    // Audio can be downloaded, but not decoded here due to browser restrictions
+
+    GameAudio = (await Promise.all(Object.entries(AUDIO).map(async ([name, url]) => {
+            const response = await fetch(url);
+            const blob = await response.arrayBuffer();
+            return [name, blob] as const;
+        })))
+        .reduce((all, [name, audio]) => {
+            all[name] = audio;
+            return all;
+        }, {} as Audio);
+
+}
+
+export function playAudio(clip: ArrayBuffer, loop = false) {
+    const context = new AudioContext();
+    const source = context.createBufferSource();
+    source.loop = loop;
+    
+    context.decodeAudioData(clip.slice(0))
+        .then(buffer => {
+            source.buffer = buffer;
+            source.connect(context.destination);
+            source.start();
+        })
+        .catch(err => {
+            console.error('audio clip playback failed');
+            console.error(err);
+        });
+}
